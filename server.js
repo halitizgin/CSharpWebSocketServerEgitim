8f86c71f6e88558be787d34a40937cddc9580ddd
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-06-23 17:23:26 +0300

    C# Websocket Ders 9 - Verileri Tüm Client'lara dağıtmak.

--- server.js
+++ server.js
@@ -6,14 +6,8 @@ http.listen(3000);
 io.on('connection', socket => {
     console.log('Kullanıcı bağlandı.');
 
-    socket.emit('hi');
-
-    socket.on('thanks', () => {
-        console.log('Hoşbulduk.');
-    });
-
-    socket.on('click', () => {
-        console.log("Tıklandı!");
+    socket.on('message', data => {
+        socket.broadcast.emit('message', data);
     });
 
     socket.on('disconnect', () => {
